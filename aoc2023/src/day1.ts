4b703c682a25e0cc8dd337cf27d9aac9fc1c148d
rmonaghanjr <rmonagha@stevens.edu>
2023-11-30 10:24:47 -0500

    create template for aoc2023 in ts

---
+++ aoc2023/src/day1.ts
@@ -0,0 +1,4 @@
+/*
+ * Ryan Monaghan
+ * Advent of Code 2023, Day 1
+ */
---
+++ aoc2023/src/index.ts
@@ -0,0 +1,10 @@
+/*
+ * Ryan Monaghan
+ * Advent of Code 2023
+ */
+
+function main() {
+    console.log("----- Advent of Code 2023 -----");
+}
+
+main();
